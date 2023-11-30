aca99d1b9e6d257812a504c5652e3f00961e0c1d
khalifahnur <khalifahnur1095@gmail.com>
2023-11-30 10:21:51 -0800

    modules

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Print sum of two numbers """
+from add_0.py import add
+
+results = add(1,2)
+print("{} + {} = {}".format(1,2,results), end='')
---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Function to print two numbers"""
+def add(a,b):
+    return a+b
