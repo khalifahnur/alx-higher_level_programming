db409514c602e7696034378c873faa1808127789
khalifahnur <khalifahnur1095@gmail.com>
2023-11-30 10:28:04 -0800

    correction

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
-"""Function to print two numbers"""
-def add(a,b):
-    return a+b
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
